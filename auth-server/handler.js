a38be0efaa2bc5065109ded822b6ce747d59b450
khaledawaza <1khaledawaza@gmail.com>
2023-04-06 02:36:54 +0200

    add url

--- auth-server/handler.js
+++ auth-server/handler.js
@@ -20,15 +20,11 @@ const credentials = {
   auth_uri: "https://accounts.google.com/o/oauth2/auth",
   token_uri: "https://oauth2.googleapis.com/token",
   auth_provider_x509_cert_url: "https://www.googleapis.com/oauth2/v1/certs",
-  redirect_uris: [
-    "https://khaledawaza.github.io/meet",
-    "http://localhost:3000",
-  ],
-  javascript_origins: [
-    "https://khaledawaza.github.io/meet",
-    "http://localhost:3000",
-  ],
+  redirect_uris: ["https://khaledawaza.github.io/meet", "http://localhost:3000"],
+  javascript_origins: ["https://khaledawaza.github.io/meet", "http://localhost:3000"],
 };
+
+
 const { client_secret, client_id, redirect_uris, calendar_id } = credentials;
 const oAuth2Client = new google.auth.OAuth2(
   client_id,
